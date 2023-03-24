b06eb6ba6a1142f4567ddafadaee385a7e27fa28
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-24 08:38:41 +0530

    added: daily wage

--- Employee.js
+++ Employee.js
@@ -1,10 +1,26 @@
 const IS_ABSENT =0;
 
-let emp_check = Math.floor(Math.random()*10)%2;
-if(emp_check==IS_ABSENT){
-    console.log("Employee is Absent");
-}
-else{
-    console.log("Present");
+const Is_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HR = 4;
+const FULL_TIME_HR = 8;
+const WAGE_PER_HR = 20;
+
+let empHrs = 0;
+
+
+let emp_check = Math.floor(Math.random()*10)%3;
+
+switch(emp_check){
+    case Is_PART_TIME:
+        empHrs = PART_TIME_HR;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HR;
+        break;
+    default:
+        empHrs=0;    
 }
 
+let empWage = empHrs * WAGE_PER_HR;
+console.log("Wage :"+empWage);
