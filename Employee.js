e202a4b3a62eb6630b6f93623e236da6e8585f94
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-24 08:33:51 +0530

    added: uc1

---
+++ Employee.js
@@ -0,0 +1,10 @@
+const IS_ABSENT =0;
+
+let emp_check = Math.floor(Math.random()*10)%2;
+if(emp_check==IS_ABSENT){
+    console.log("Employee is Absent");
+}
+else{
+    console.log("Present");
+}
+
